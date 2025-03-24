3b19be1f96e5f0e1d9f60e316bfc4a75c8b1b521
n00nchi <alfredooviedo@Mac.lan>
2025-03-23 20:37:36 -0500

    command not found duh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,6 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     String input = scanner.nextLine();
 
-    System.out.println("Error: command nor recognized");
+    System.out.println(input+ ": " + "command not found");
   }
 }
