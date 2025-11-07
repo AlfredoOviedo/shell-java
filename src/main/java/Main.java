4d5c9c0d42de0b2509f32ab9512db7cc85308883
n00nchi <alfredooviedo@Alfredos-MacBook-Pro.local>
2025-11-06 21:11:23 -0600

    added exit to shell

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,21 @@ public class Main {
   public static void main(String[] args) throws Exception {
     
     Scanner scanner = new Scanner(System.in);
-  
-    while (true)  {
-       
-    System.out.print("$ ");
-    String input = scanner.nextLine();    
 
+    while (true)  {
+    System.out.print("$ ");
+    String input = scanner.nextLine();
+    if (input.equalsIgnoreCase("exit")) {
+      System.out.println("0");
+      break;
+    }       
       try {
-        System.out.println(input+ ": " + "command not found"); 
-        } catch (Exception e) {
-        System.err.println("Error: " + e.getMessage());
-        }
+            System.out.println(input+ ": " + "command not found"); 
+          } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+
       }
+    }
+    scanner.close();
   }
 }
